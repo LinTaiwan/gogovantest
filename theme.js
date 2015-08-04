35f187912f94656d1baf09e9cf2ed362555a2dbf
LinTaiwan <frank613055@gmail.com>
2015-08-04 22:34:08 +0800

    ss

---
+++ theme.js
@@ -0,0 +1,31 @@
+/* Copyright (C) YOOtheme GmbH, YOOtheme Proprietary Use License (http://www.yootheme.com/license) */
+
+jQuery(function($) {
+
+	var config = $('html').data('config') || {};
+
+$(".pagination-start").closest("ul").addClass("uk-pagination");
+
+	// Social buttons
+	$('article[data-permalink]').socialButtons(config);
+
+    // Fixed headerbar
+
+    var doc     = $(document),
+        header  = $('.tm-headerbar');
+
+    if ($('body').hasClass('tm-navbar-fixed')) {
+
+        doc.on('scroll', function(){
+
+            if(doc.scrollTop() > 100){
+                header.addClass('tm-headerbar-small');
+            } else{
+                header.removeClass('tm-headerbar-small');
+            }
+
+        });
+
+    }
+
+});
